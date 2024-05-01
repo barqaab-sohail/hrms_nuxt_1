17e4f9544fcbff162b867313848a6dae7c26ab06
Sohail Afzal <sohail.afzal.com>
2024-05-01 07:52:47 +0500

    01-05-2024

---
+++ pages/assets/subClass/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute();
+console.log('sub class ic '+route.params.id );
+</script>
+
+<template>
+    <div>
+        <h1>Asset Sub Class Detail</h1>
+    </div>
+</template>
